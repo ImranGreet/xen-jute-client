01e7b2cfbc422eff398790a972a2757ff2412289
unknown <hossainimran1208@gmail.com>
2025-10-06 22:18:21 +0600

    git init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import '../assets/scss/style.scss'; 
+
+	let { children } = $props();
+	let defaultTitle:string = 'Xen Jute' ;
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>{defaultTitle}</title>
+</svelte:head>
+
+{@render children?.()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<h1>Welcome to SvelteKit <span>REd</span> </h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button class="btn">Add to cart</button>
+
+<style lang="scss">
+    h1 {
+        color: purple;
+        font-size: 2em;
+        &:hover{
+            background-color: red;
+            transition: 0.53s all ease-in-out;
+            & span {
+                color: white;
+            }
+        }
+    }
+</style>
